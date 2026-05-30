e55b3db3fea4ce6e3b6f11af396c6eb00c6de423
tushu <ritiksingh7055072196>
2026-05-30 18:00:34 +0530

    final update

---
+++ 2026/05-May-2026/2026-05-27-Count the Number of Special Characters II.java
@@ -0,0 +1,45 @@
+// class Solution {
+
+//     public int numberOfSpecialChars(String word) {
+
+//         int[] lower = new int[26];
+//         int[] upper = new int[26];
+
+//         Arrays.fill(lower, -1);
+//         Arrays.fill(upper, -1);
+
+//         // Store positions
+//         for (int i = 0; i < word.length(); i++) {
+
+//             char ch = word.charAt(i);
+
+//             if (Character.isLowerCase(ch)) {
+
+//                 lower[ch - 'a'] = i;
+
+//             } else {
+
+//                 int idx = ch - 'A';
+
+//                 if (upper[idx] == -1) {
+//                     upper[idx] = i;
+//                 }
+//             }
+//         }
+
+//         int count = 0;
+
+//         // Check all characters
+//         for (int i = 0; i < 26; i++) {
+
+//             if (lower[i] != -1 &&
+//                 upper[i] != -1 &&
+//                 lower[i] < upper[i]) {
+
+//                 count++;
+//             }
+//         }
+
+//         return count;
+//     }
+// }
