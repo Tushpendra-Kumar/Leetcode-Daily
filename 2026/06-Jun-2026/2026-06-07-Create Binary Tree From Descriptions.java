94b6da25d6bf202173e825484e3a8121487bb200
tushu <ritiksingh7055072196>
2026-06-07 09:24:12 +0530

    final update

--- 2026/06-Jun-2026/2026-06-07-Create Binary Tree From Descriptions.java
+++ 2026/06-Jun-2026/2026-06-07-Create Binary Tree From Descriptions.java
@@ -1,52 +1,52 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public TreeNode createBinaryTree(int[][] descriptions) {
-        Map<Integer, TreeNode> map = new HashMap<>();
-        Set<Integer> children = new HashSet<>();
+// /**
+//  * Definition for a binary tree node.
+//  * public class TreeNode {
+//  *     int val;
+//  *     TreeNode left;
+//  *     TreeNode right;
+//  *     TreeNode() {}
+//  *     TreeNode(int val) { this.val = val; }
+//  *     TreeNode(int val, TreeNode left, TreeNode right) {
+//  *         this.val = val;
+//  *         this.left = left;
+//  *         this.right = right;
+//  *     }
+//  * }
+//  */
+// class Solution {
+//     public TreeNode createBinaryTree(int[][] descriptions) {
+//         Map<Integer, TreeNode> map = new HashMap<>();
+//         Set<Integer> children = new HashSet<>();
 
-        for (int[] d : descriptions) {
+//         for (int[] d : descriptions) {
 
-            int parent = d[0];
-            int child = d[1];
-            int isLeft = d[2];
+//             int parent = d[0];
+//             int child = d[1];
+//             int isLeft = d[2];
 
-            map.putIfAbsent(parent, new TreeNode(parent));
-            map.putIfAbsent(child, new TreeNode(child));
+//             map.putIfAbsent(parent, new TreeNode(parent));
+//             map.putIfAbsent(child, new TreeNode(child));
 
-            TreeNode parentNode = map.get(parent);
-            TreeNode childNode = map.get(child);
+//             TreeNode parentNode = map.get(parent);
+//             TreeNode childNode = map.get(child);
 
-            if (isLeft == 1) {
-                parentNode.left = childNode;
-            } else {
-                parentNode.right = childNode;
-            }
+//             if (isLeft == 1) {
+//                 parentNode.left = childNode;
+//             } else {
+//                 parentNode.right = childNode;
+//             }
 
-            children.add(child);
-        }
+//             children.add(child);
+//         }
 
-        for (int[] d : descriptions) {
-            int parent = d[0];
+//         for (int[] d : descriptions) {
+//             int parent = d[0];
 
-            if (!children.contains(parent)) {
-                return map.get(parent);
-            }
-        }
+//             if (!children.contains(parent)) {
+//                 return map.get(parent);
+//             }
+//         }
 
-        return null;
-    }
-}
+//         return null;
+//     }
+// }
