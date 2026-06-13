790c46a15c7d6431d8b89f7b2b65c4f2e8af7559
tushu <ritiksingh7055072196>
2026-06-14 03:43:36 +0530

    today save the problem

--- 2026/06-Jun-2026/2026-06-14-Maximum Twin Sum of a Linked List.java
+++ 2026/06-Jun-2026/2026-06-14-Maximum Twin Sum of a Linked List.java
@@ -8,34 +8,34 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
-    public int pairSum(ListNode head) {
-        // Find middle
-        ListNode slow = head;
-        ListNode fast = head;
+// class Solution {
+//     public int pairSum(ListNode head) {
+//         // Find middle
+//         ListNode slow = head;
+//         ListNode fast = head;
 
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        // Reverse second half
-        ListNode prev = null;
-        while (slow != null) {
-            ListNode next = slow.next;
-            slow.next = prev;
-            prev = slow;
-            slow = next;
-        }
-        // Calculate maximum twin sum
-        int ans = 0;
-        ListNode first = head;
-        ListNode second = prev;
+//         while (fast != null && fast.next != null) {
+//             slow = slow.next;
+//             fast = fast.next.next;
+//         }
+//         // Reverse second half
+//         ListNode prev = null;
+//         while (slow != null) {
+//             ListNode next = slow.next;
+//             slow.next = prev;
+//             prev = slow;
+//             slow = next;
+//         }
+//         // Calculate maximum twin sum
+//         int ans = 0;
+//         ListNode first = head;
+//         ListNode second = prev;
 
-        while (second != null) {
-            ans = Math.max(ans, first.val + second.val);
-            first = first.next;
-            second = second.next;
-        }
-        return ans;
-    }
-}
+//         while (second != null) {
+//             ans = Math.max(ans, first.val + second.val);
+//             first = first.next;
+//             second = second.next;
+//         }
+//         return ans;
+//     }
+// }
